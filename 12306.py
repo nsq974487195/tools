5979d34bf76133b2208b01e351fd9672a7f68974
nsq@xjtu <nsqiang@gmail.com>
2015-12-10 21:39:13 +0800

    init

---
+++ 12306.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from splinter import Browser
+
+with Browser() as browser:
+	url="https://kyfw.12306.cn/otn/leftTicket/init"
+	browser.visit(url)
+	browser.fill('fromStationText',"splinter")
+	#button=browser.find_by_id(u"su")
+	#button.click()
+	if browser.is_text_present('splinter'):
+		print "sucess"
+	else:
+		print"error"
